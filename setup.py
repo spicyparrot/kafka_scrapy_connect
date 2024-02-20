8a5a666052ea46a3b705ac078c1692604f85fe3f
jdickson1992 <jspmck1990@gmail.com>
2024-02-20 14:25:48 +0000

    feat: adding workflow for creating releases/tags

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README_MD = open(os.path.join(MY_DIR, 'README.md')).read()
 
 setup(
     name='kafka-scrapy-connect',
-    version='1.0.1',
+    version='{{VERSION_PLACEHOLDER}}',
     description='Integrating Scrapy with kafka using the confluent-kafka python client',
     long_description=README_MD,
     long_description_content_type="text/markdown",

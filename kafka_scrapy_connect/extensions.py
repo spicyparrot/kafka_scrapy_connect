dfb3fc994cf0a6634adc23708d21b1dd816bae2b
jdickson1992 <jspmck1990@gmail.com>
2024-02-27 15:53:12 +0000

    feat: only output http status codes that have counts > 0

--- kafka_scrapy_connect/extensions.py
+++ kafka_scrapy_connect/extensions.py
@@ -197,8 +197,17 @@ class KafkaLogStats:
         """
         Get HTTP response status counts.
         """
-        status_codes = ['200', '201', '202', '204', '300', '301', '302', '303', '400', '401', '403']
-        return {code: self.stats.get_stats().get(f"downloader/response_status_count/{code}", 0) for code in status_codes}
+        status_codes = [
+            '200', '201', '202', '203', '204', '205', '206', '207', '208', '226',
+            '300', '301', '302', '303', '304', '305', '306', '307', '308',
+            '400', '401', '402', '403', '404', '405', '406', '407', '408', '409', '410', '411', '412', '413', '414', '415', '416', '417', '418', '421', '422', '423', '424', '425', '426', '429', '431', '451'
+            ]
+        status_counts = {}
+        for code in status_codes:
+            count = self.stats.get_stats().get(f"downloader/response_status_count/{code}",0)
+            if count > 0:
+                status_counts[code] = count
+        return status_counts
 
     def reset_stats(self):
         """

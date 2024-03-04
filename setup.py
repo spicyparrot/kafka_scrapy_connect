4d5944de7f288cf019e208f6df10364b289b8204
James <47530786+jdickson1992@users.noreply.github.com>
2024-03-04 22:24:20 +0000

    feat: Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     url='https://github.com/spicyparrot/kafka_scrapy_connect',
     classifiers = [
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
